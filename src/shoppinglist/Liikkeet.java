5c4191cbaf3c93094396703f32fe552ad3c93bf2
kankaazt <akselik11@gmail.com>
2021-02-26 12:51:28 +0200

    säätöä

---
+++ src/shoppinglist/Liikkeet.java
@@ -0,0 +1,16 @@
+package shoppinglist;
+
+public class Liikkeet {
+
+   /** public static void main(String[] args) {
+        Liikkeet liikkeet = new Liikkeet();
+        Citymarket.rekisteroi();
+        Prisma.rekisteroi();
+        Citymarket.vastaaCitymarket();
+        Prisma.vastaaPrisma();
+        
+        liikkeet.Lisaa(Citymarket);
+        liikkeet.Lisaa(Prisma);
+        **/
+    
+}
